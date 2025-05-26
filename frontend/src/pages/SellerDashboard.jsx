b800e8eec009ed2d5e5bc9140674d8b8f37d4503
Hüseyin Aslan <btdhaslan@gmail.com>
2025-05-26 18:13:29 +0300

    sideBar width değeri değiştirildi

--- frontend/src/pages/SellerDashboard.jsx
+++ frontend/src/pages/SellerDashboard.jsx
@@ -1,9 +1,12 @@
 import SellerSidebar from '../components/SellerSidebar';
-
+import LanguageSelector from '../components/LanguageSelector';
 const SellerDashboard = () => {
   return (
     <div className="flex">
       <SellerSidebar />
+       <div className="language-selector-container">
+        <LanguageSelector />
+      </div>
       <div className="flex-1 p-4 ml-[30%] max-w-[70%]">
         <h1 className="text-2xl font-bold mb-4">Mağaza Paneli</h1>
         {/* İleride buraya route’a göre içerikler gelecek */}
