7c3b36a93bd497ad2433fe570d8f4823bbdb896a
Hüseyin Aslan <btdhaslan@gmail.com>
2025-05-26 18:59:53 +0300

    sidebar çeviri işlemleri tamamlandı

--- frontend/src/components/SellerSidebar.jsx
+++ frontend/src/components/SellerSidebar.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import { useTranslation } from 'react-i18next';
 import {
   ChevronLeft, ChevronRight, Building2, PackagePlus,
   BarChart3, Store, CheckCircle, CircleDollarSign
@@ -8,6 +9,7 @@ import {
 import '../styles/SellerSidebar.css';
 
 const SellerSidebar = () => {
+   const { t } = useTranslation();
   const [isOpen, setIsOpen] = useState(true);
   const [openDropdown, setOpenDropdown] = useState(null);
   const [seller, setSeller] = useState(null);
@@ -41,7 +43,7 @@ const SellerSidebar = () => {
 
       <div className={`seller-sidebar ${isOpen ? 'open' : 'closed'}`}>
         <div className="sidebar-header">
-          <span>{seller?.user?.name || 'Yükleniyor...'}</span>
+          <span>{seller?.user?.name || 'Loading...'}</span>
           <button onClick={toggleSidebar} className="toggle-button">
             <ChevronLeft size={20} />
           </button>
@@ -51,45 +53,45 @@ const SellerSidebar = () => {
           {/* Mağaza */}
           <div className="sidebar-section marginBott1em">
             <div className="section-header marginBott1em" onClick={() => toggleDropdown('store')}>
-              <Store size={25} /> <span className='bigger-icon'>Mağaza</span>
+              <Store size={25} /> <span className='bigger-icon'>{t('sellerSidebar.store')}</span>
             </div>
             {openDropdown === 'store' && (
               <div className="section-items">
                 <button  onClick={() => navigate('/seller/company/create')} className='marginBott1em'>
-                  <Building2 size={20} /> <span className='bigger-icon'>Şirketini Oluştur</span>
+                  <Building2 size={20} /> <span className='bigger-icon'>{t('sellerSidebar.createCompany')}</span>
                 </button>
                 <button onClick={() => navigate('/seller/company/info')} className='marginBott1em'>
-                  <Building2 size={20} /> <span className='bigger-icon'>Şirket Bilgileri</span>
+                  <Building2 size={20} /> <span className='bigger-icon'>{t('sellerSidebar.companyInfo')}</span>
                 </button>
                 <button onClick={() => navigate('/seller/products/add')} className='marginBott1em'>
-                  <PackagePlus size={20} /> <span className='bigger-icon'>Ürün Ekle</span>
+                  <PackagePlus size={20} /> <span className='bigger-icon'>{t('sellerSidebar.addProduct')}</span>
                 </button>
                 <button onClick={() => navigate('/seller/products')}>
-                  <PackagePlus size={20} /> <span className='bigger-icon' >Ürünleri Düzenle</span>
+                  <PackagePlus size={20} /> <span className='bigger-icon' >{t('sellerSidebar.editProducts')}</span>
                 </button>
               </div>
             )}
           </div>
 
             <div className="section-header marginBott1em" onClick={() => navigate('/seller/statistics')}>
-              <Store size={25} /> <span className='bigger-icon'>Şirket İstatistikleri</span>
+              <Store size={25} /> <span className='bigger-icon'>{t('sellerSidebar.statistics')}</span>
             </div>
 
           {/* Planlar */}
           <div className="marginBott1em">
             <div className="section-header marginBott1em" onClick={() => toggleDropdown('plans')}>
-              <CircleDollarSign size={25} /> <span>Planlar</span>
+              <CircleDollarSign size={25} /> <span>{t('sellerSidebar.plans')}</span>
             </div>
             {openDropdown === 'plans' && (
               <div className="section-items">
                 <button onClick={() => navigate('/seller/plans/free')} className='marginBott1em'>
-                  <CheckCircle size={20} /> <span className='bigger-icon'>Free Plan</span>
+                  <CheckCircle size={20} /> <span className='bigger-icon'>{t('sellerSidebar.freePlan')}</span>
                 </button>
                 <button onClick={() => navigate('/seller/plans/premium')} className='marginBott1em'>
-                  <CheckCircle size={20} /> <span className='bigger-icon'>Premium Plan</span>
+                  <CheckCircle size={20} /> <span className='bigger-icon'>{t('sellerSidebar.premiumPlan')}</span>
                 </button>
                 <button onClick={() => navigate('/seller/plans/business')} className='marginBott1em'>
-                  <CheckCircle size={20} /> <span className='bigger-icon'>Business Plan</span>
+                  <CheckCircle size={20} /> <span className='bigger-icon'>{t('sellerSidebar.businessPlan')}</span>
                 </button>
               </div>
             )}
